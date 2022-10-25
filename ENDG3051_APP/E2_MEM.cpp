e765b2bb6f659e92a3b46020949785a06065decc
Stephan Kolontay <stepan.kolontaj@gmail.com>
2022-10-25 12:55:56 +0100

    Edited write function

--- ENDG3051_APP/E2_MEM.cpp
+++ ENDG3051_APP/E2_MEM.cpp
@@ -11,64 +11,75 @@
 |*| @description: parameterized constructor that takes address of E2 device from the board;
 |*| @PARAM: _port_addr describes the type of wiring and ranges from 0-7
 \*/
-E2_MEM::E2_MEM(uint8_t _port_addr, int _mrs){
-  this->_IIC_E2_ADDR_ = 0xA0 | _port_addr << 1; //control bit + port address
-  this->_max_rec_size_ = _mrs;
-  Kernel::OS.IICDriver.IICWrite(_IIC_E2_ADDR_, 0x0, 1); //write address
+E2_MEM::E2_MEM(uint8_t _port_addr, uint8_t _mrsB)
+{
+  this->_IIC_E2_ADDR_ = 0xA0 | _port_addr << 1; // control bit + port address
+  this->_max_rec_size_ = _mrsB;
+  Kernel::OS.IICDriver.IICWrite(_IIC_E2_ADDR_, 0x0, 1); // write address
 }
 
 /*\ ---------------------------------------------
 |*| @name: sizeof_cchp
-|*| @description: 
+|*| @description:
 \*/
-uint16_t E2_MEM::sizeof_cchp(const cahr* _pch){
-    uint16_t _it = 0;
-    while (_pch[_it] != '\0') _it++;
-    return _it;
+uint8_t E2_MEM::sizeof_cchp(const cahr *_pch)
+{
+  uint16_t _it = 0;
+  while (_pch[_it] != '\0')
+    _it++;
+  return _it;
 }
 
 /*\ ---------------------------------------------
 |*| @name: Write
-|*| @description: 
+|*| @description:
 \*/
-int E2_MEM::Write(uint16_t _addr, const char* _buffer){
-  struct pagewrite{
+int E2_MEM::Write(uint16_t _addr, const char *_data)
+{
+  page_rw
+  {
     unsigned char addr[2];
-    unsigned char data[128];
-  }wr;
-  
-  auto _buffer_size = sizeof_cchp(_buffer);
+    unsigned char data[this->max_rec_size_B];
+  }
+  write;
+
+  auto _data_size = sizeof_cchp(_data);
   int _it = 0;
-  
-  //check if address is empty, then write --> output address location
-  // else : skip to empty address --> output address location
-  
-  do{
-  wr.addr[0] = (_addr >> 8) & 0xFF;
-  wr.addr[1] =  _addr & 0x0FF;
 
-  for(int i = 0; i < this->max_rec_size_B; i++)
-     wr.data[i] = _buffer[i + (this->max_rec_size_B * _it)];
+  // check if address is empty, then write --> output address location
+  //  else : skip to empty address --> output address location
 
-   Kernel::OS.IICDriver.IICWrite(_IIC_E2_ADDR_, (unsigned char *)&wr, this->_max_rec_size_ + 2);
-   
-    _buffer_size -= max_rec_size_B;
+  do
+  {
+    write.addr[0] = (_addr >> 8) & 0xFF;
+    write.addr[1] = _addr & 0x0FF;
+
+    for (int i = 0; i < this->max_rec_size_B; i++)
+      write.data[i] = _data[i + (this->max_rec_size_B * _it)];
+
+    Kernel::OS.IICDriver.IICWrite(_IIC_E2_ADDR_, (unsigned char *)&write, this->max_rec_size_B + 2);
+
+    _data_size -= max_rec_size_B;
     _addr++;
     _it++;
-  } while(_buffer_size > 0);
+  } while (_data_size > 0);
+
+  return Serial.write(write.data, _data_size) // temp;
 }
 
 /*\ ---------------------------------------------
 |*| @name: Read
-|*| @description: 
+|*| @description:
 \*/
-int E2_MEM::Read(uint16_t _addr, unsigned char* _buffer){
+int E2_MEM::Read(uint16_t _addr, uint8_t _data_size)
+{
 
-
-  
-  //unsigned char addr[2] = { (_address>>8)& 0xff, _address & 0xff };  
-  //unsigned char databuffer[400];
-  //int datalen = 300;  
-  //Kernel::OS.IICDriver.IICWrite(IIC_CTRb_ADDR_E2, addr, 2)
-  //Kernel::OS.IICDriver.IICRead(IIC_CTRb_ADDR_E2, databuffer, datalen)
+  // unsigned char addr[2] = { (_address>>8)& 0xff, _address & 0xff };
+  // unsigned char databuffer[400];
+  // int datalen = 300;
+  // Kernel::OS.IICDriver.IICWrite(IIC_CTRb_ADDR_E2, addr, 2)
+  // Kernel::OS.IICDriver.IICRead(IIC_CTRb_ADDR_E2, databuffer, datalen)
+  return 0;
 }
+
+int E2_MEM::Read(struct page_rw *_read) {}
--- ENDG3051_APP/E2_MEM.h
+++ ENDG3051_APP/E2_MEM.h
@@ -2,10 +2,10 @@
 |*| @name: E2_MEM.H
 |*| @author: Stephan Kolontay 2022
 |*| @date: 18/10/2022
-|*| @description: Derived from Task - this uses a non-volatile E2 data logger to 
+|*| @description: Derived from Task - this uses a non-volatile E2 data logger to
 |*| maintain a recording of events that have taken place
 |*| @Note: max size: 500 pages (64000b | 128B/page)
-\*/ 
+\*/
 
 #ifndef E2_MEM_H_
 #define E2_MEM_H_
@@ -15,23 +15,22 @@
 #define E2_base_addr 0x0
 #define E2_end_addr 0xFA00
 
-class E2_MEM { 
-  
+class E2_MEM
+{
+
   struct page_rw;
   uint8_t _IIC_E2_ADDR_;
-  uint16_t sizeof_cchp(const char* _pch);
-  uint16_t  max_rec_size_B; //max record size in bytes ()
-	
+  uint8_t sizeof_cchp(const char *_pch);
+  uint8_t max_rec_size_B; // max record size in bytes / max 64B
+
 public:
- 
-  E2_MEM(uint8_t _port_addr = 0, int _mrsB = 16); //init slave eeprom's address //doesnt have unique value check (may crush on the bus);
+  E2_MEM(uint8_t _port_addr = 0, uint8_t _mrsB = 16); // init slave eeprom's address //doesnt have unique value check (may crush on the bus);
   ~E2_MEM(){};
-  
-  int Write(uint16_t _addr, const char* _data);
-  
-   int Read(uint16_t _addr,  uint16_t _data_size);
-   int Read(struct page_rw* r) // takes &w
-		
+
+  int Write(uint16_t _addr, const char *_data);
+
+  int Read(uint16_t _addr, uint8_t _data_size);
+  int Read(struct page_rw *_read) // instant read / takes &w
 };
 
 #endif
